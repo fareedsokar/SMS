b5f96f6bf8589c7a64545c8b9c0526a18aa0218a
fareedsokar <the.angle.f@gmail.com>
2017-06-11 12:55:01 +0300

    Mohamad Hosary

---
+++ SMS/src/User/LoginController.java
@@ -0,0 +1,25 @@
+package User;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+
+public class LoginController{// implements Initializable{
+
+	
+}
